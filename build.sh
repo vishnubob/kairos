e3fcbf4f42810e1ef78f2b7d4e96c24c83ffaabd
Giles Hall <giles@polymerase.org>
2017-10-07 23:03:36 -0400

    checkin arduino build shims

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+build_dir=/tmp/__arduino_build
+if [ -d $build ]; then
+	rm -rf $build_dir
+fi
+mkdir -p ${build_dir}
+
+/usr/share/arduino/arduino-builder \
+	-hardware /usr/share/arduino/hardware \
+	-tools /usr/share/arduino/hardware/tools \
+	-tools /usr/share/arduino/tools-builder \
+	-libraries $(pwd)/libraries \
+	-fqbn arduino:avr:uno \
+	-verbose \
+	-build-path ${build_dir} \
+	$(pwd)
+
+cp ${build_dir}/*.hex .
--- flashphoto.ino
+++ flashphoto.ino
@@ -4,38 +4,35 @@ ISR (ANALOG_COMP_vect)
 {
    // clk / 1
    // CTC mode, OCR1A configured as top
-   TCCR1B = bit(CS10) | bit(WGM12);
-   ACSR = 0;
+   //TCCR1B = bit(CS10) | bit(WGM12);
+   //ACSR = 0;
+   triggered = true;
 }
 
 void arm()
 {
-  ADCSRB = 0;           // (Disable) ACME: Analog Comparator Multiplexer Enable
-  ACSR =  bit (ACI)     // (Clear) Analog Comparator Interrupt Flag
-        | bit (ACIE)    // Analog Comparator Interrupt Enable
-        | bit (ACIS1)
-        | bit (ACIS0);
-  TCCR1B = 0;
-   // set OC1A to high on CTC match
-   TCCR1A = bit(COM1A1) | bit(COM1A0); 
-   TCNT1 = 0;
-   OCR1A = 200;
+    ADCSRB = 0;
+    ACSR =  bit(ACI) | bit(ACIE) | bit(ACIS1);
+    //TCCR1B = 0;
+    // set OC1A to high on CTC match
+    //TCCR1A = bit(COM1A1) | bit(COM1A0); 
+    //TCNT1 = 0;
+    //OCR1A = 200;
 }
 
 void setup ()
 {
-  Serial.begin (115200);
-  Serial.println ("Started.");
- }
+    Serial.begin (115200);
+    Serial.println ("Started.");
+}
 
 void loop ()
 {
-  if (triggered)
-  {
-      unsigned long time = millis();
-      Serial.print(time);
-      Serial.println(" - Triggered!"); 
-      triggered = false;
-      ACSR = bit (ACI) | bit (ACIE) | bit (ACIS1) | bit(ACIS0);
-  }
+    if (triggered)
+    {
+        unsigned long time = millis();
+        Serial.print(time);
+        Serial.println(" - Triggered!"); 
+        triggered = false;
+    }
 }
